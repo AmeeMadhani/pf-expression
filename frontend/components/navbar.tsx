829e3c1f3712619aaee54c7f2225aa3881654450
Amee Madhani <e21cseu0171@bennett.edu.in>
2024-05-01 19:42:16 +0530

    update frontend

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,95 +1,36 @@
+import React from "react";
 import {
-	Navbar as NextUINavbar,
-	NavbarContent,
-	NavbarMenu,
-	NavbarMenuToggle,
-	NavbarBrand,
-	NavbarItem,
-	NavbarMenuItem,
-} from "@nextui-org/navbar";
-
-import { Link } from "@nextui-org/link";
-import { link as linkStyles } from "@nextui-org/theme";
-
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
+// import {AcmeLogo} from "./AcmeLogo.jsx";
+import { GithubIcon } from "@/components/icons";
 import { siteConfig } from "@/config/site";
-import NextLink from "next/link";
-import clsx from "clsx";
-
-import {
-	GithubIcon,
-} from "@/components/icons";
 
 import { Logo } from "@/components/icons";
 
-export const Navbar = () => {
-	return (
-		<NextUINavbar isBordered maxWidth="xl" position="sticky">
-			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-				<NavbarBrand as="li" className="gap-3 max-w-fit">
-					<NextLink className="flex justify-start items-center gap-1 text-xl" href="/">
-						<Logo style={{ width: '50px', height: '50px' }} />
+export const Navbarr = () => {
+  return (
+    <Navbar className="light text-black">
+      <NavbarBrand>
+        <Logo style={{ width: "50px", height: "50px" }} />
 
-						<p> PetFacial </p>
-					</NextLink>
-				</NavbarBrand>
-			</NavbarContent>
-
-			<NavbarContent
-				className="hidden sm:flex basis-1/5 sm:basis-full"
-				justify="end"
-			>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
-					))}
-				</ul>
-
-				<NavbarItem className="hidden sm:flex gap-2">
-					<Link isExternal href={siteConfig.links.github} aria-label="Github">
-						<GithubIcon className="text-default-500" />
-					</Link>
-				</NavbarItem>
-			</NavbarContent>
-
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-				<Link isExternal href={siteConfig.links.github} aria-label="Github">
-					<GithubIcon className="text-default-500" />
-				</Link>
-				<NavbarMenuToggle />
-			</NavbarContent>
-
-			<NavbarMenu>
-				<div className="mx-4 mt-2 flex flex-col gap-2">
-					{siteConfig.navMenuItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									index === 2
-										? "danger"
-										: index === siteConfig.navMenuItems.length - 1
-											? "danger"
-											: "foreground"
-								}
-								href="#"
-								size="lg"
-							>
-								{item.label}
-							</Link>
-						</NavbarMenuItem>
-					))}
-				</div>
-			</NavbarMenu>
-		</NextUINavbar>
-	);
+        <p className="font-bold text-inherit">PET FACIAL</p>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Algorithm</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link isExternal href={siteConfig.links.github} aria-label="Github">
+            <GithubIcon className="text-default-500" />
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 };
